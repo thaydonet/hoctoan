dc5b4bfeb64aadedfb2d3ce1d1c963fb3886e3d4
thaydonet <thaydo.net@gmail.com>
2025-07-29 21:25:21 +0700

    Update mathChapters.ts

--- src/data/mathChapters.ts
+++ src/data/mathChapters.ts
@@ -1,6 +1,7 @@
 import { Chapter } from '../types/MathTopic';
 import { c1b1PhanThucDaiSo } from '../../data/lessons/c1-b1-phan-thuc-dai-so';
 import { c1b2PhepCongTru } from '../../data/lessons/c1-b2-phep-cong-tru';
+import { c3b1pythagore } from '../../data/lessons/c3-b1-dinh-ly-pythagore';
 
 export const mathChapters: Chapter[] = [
   {
@@ -23,7 +24,9 @@ export const mathChapters: Chapter[] = [
     id: 'chuong-3',
     title: 'Chương 3: Định lí Pythagore - Các loại tứ giác thường gặp',
     description: 'Định lí Pythagore - Các loại tứ giác thường gặp',
-    lessons: []
+    lessons: [
+      c3b1pythagore,
+    ]
   },
   {
     id: 'chuong-4',
